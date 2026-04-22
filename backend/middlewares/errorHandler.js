d6b7e817708533e3aeb62c8804021be8f12c4a33
rohinimi <rohini.mittal.it.2018@miet.ac.in>
2026-04-22 12:19:01 +0530

    updated one

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -1,6 +1,3 @@
 export const errorHandler = (err, req, res, next) => {
-    if (res.headersSent) {
-        return next(err); // 🔥 important
-    }
     res.status(500).json({ message: err.message||"Internal Error" });
 };
--- backend/middlewares/pathHandlerMiddleware.js
+++ backend/middlewares/pathHandlerMiddleware.js
@@ -1,4 +1,3 @@
-export const pathHandlerMiddleware = (req,res,next)=>{
-    res.send(404).json({message:"PAGE NOT FOUND"})
-    next();
+export const pathHandlerMiddleware = (req,res)=>{
+    res.status(404).json({message:"PAGE NOT FOUND"})
 }
